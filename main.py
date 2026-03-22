bf279e9dfe332a444fd474d4a6c4a4fb0536f5e4
joao-vieira21 <joao2017ig@gmail.com>
2026-03-22 19:55:07 -0300

    Criação da API, primeiro prototipo

---
+++ main.py
@@ -0,0 +1,71 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import json
+from dotenv import load_dotenv
+import os
+
+app = FastAPI()
+
+load_dotenv()
+
+google_key = os.getenv("GOOGLE_KEY")
+if not google_key:
+    raise ValueError("GOOGLE_KEY não encontrada")
+
+key = json.loads(google_key)
+
+token = os.getenv("TOKEN")
+if not token:
+    raise ValueError("TOKEN não encontrado")
+
+scopes = [
+    "https://spreadsheets.google.com/feeds",
+    "https://www.googleapis.com/auth/drive",
+]
+
+creds = ServiceAccountCredentials.from_json_keyfile_dict(key, scopes=scopes)
+client = gspread.authorize(creds)
+
+planilha_completa = client.open(
+    title="monitor_temp",
+    folder_id="1xntTqkJ_jnZfvRpB3y5fHqyuOs5fxWr_"
+)
+
+aba = planilha_completa.get_worksheet(0)
+
+def nova_linha(ID, data, hora, temperatura, potencia):
+    proxima_row = len(aba.get_all_values()) + 1
+    aba.update(
+        f"A{proxima_row}:E{proxima_row}",
+        [[ID, data, hora, temperatura, potencia]]
+    )
+
+class Leitura(BaseModel):
+    ID: str
+    data: str
+    hora: str
+    temperatura: float
+    potencia: float
+    token: str
+
+@app.get("/")
+def raiz():
+    return {"status": "ok"}
+
+@app.post("/nova-leitura")
+def receber_leitura(leitura: Leitura):
+    if leitura.token != token:
+        raise HTTPException(status_code=401, detail="Acesso negado")
+
+    nova_linha(
+        leitura.ID,
+        leitura.data,
+        leitura.hora,
+        leitura.temperatura,
+        leitura.potencia
+    )
+
+    return {"status": "ok"}
+
